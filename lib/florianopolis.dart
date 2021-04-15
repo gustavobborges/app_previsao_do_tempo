54a5e2d9b1949c3cf51acdef0f977bc10d6de21d
Gustavo Borges <gustavoborges98@hotmail.com>
2021-04-14 22:04:46 -0300

    dados vindos da api

--- lib/florianopolis.dart
+++ lib/florianopolis.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'utils.dart';
+import 'dart:convert';
 
 class Florianopolis extends StatefulWidget {
   @override
@@ -7,38 +9,69 @@ class Florianopolis extends StatefulWidget {
 }
 
 class _FlorianopolisState extends State<Florianopolis> {
+  Future<Map> _getData() async {
+    http.Response response =
+        await http.get("https://api.hgbrasil.com/weather?woeid=455861");
+    return json.decode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _getData(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+          case ConnectionState.none:
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text("LOADING DATA", style: TextStyle(fontSize: 50)),
+              ],
+            );
+          default:
+            if (snapshot.hasError) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text("ERROR LOADING DATA", style: TextStyle(fontSize: 50)),
+                ],
+              );
+            }
+            return myColumn(snapshot.data);
+        }
+      },
+    );
+  }
+
+  Widget myColumn(Map data) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Padding(padding: EdgeInsets.only(top: 100),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(   
-              Icons.cloud,
-              size: 150, 
-              color: Colors.white
-            )
-          ],
-        )),
+        Padding(
+            padding: EdgeInsets.only(top: 100),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Icon(Icons.cloud, size: 150, color: Colors.white)],
+            )),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Column(
               children: [
-                TitleCity("Florianópolis"),
-                SubTitle("Ensolarado"),
+                TitleCity(data["results"]["city"]),
+                SubTitle(data["results"]["description"]),
               ],
             ),
           ],
         ),
         Column(
           children: [
-            RowPadrao("Temperatura", "35º", "Humidade", "68%"),
-            RowPadrao("Vel. Vento", "12 Km/H", "Direção", "Sul"),
-            RowPadrao("Nascer do Sol", "06:23", "Pôr do Sol", "18:11"),
+            RowPadrao("Temperatura", data["results"]["temp"].toString()+"ºC", "Humidade", data["results"]["humidity"].toString()+"%"),
+            RowPadrao("Vel. Vento", data["results"]["wind_speedy"], "Direção", "Sul"),
+            RowPadrao("Nascer do Sol",data["results"]["sunrise"], "Pôr do Sol", data["results"]["sunset"]),
           ],
         ),
       ],
